30abef7fd89d0e938efef5a790bd41d08ba576d4
Nick Crews <nickcrews35@gmail.com>
2022-05-08 00:31:57 -0400

    Added test/deployment scripts for new contracts

---
+++ scripts/standard-storage/deploy.js
@@ -0,0 +1,22 @@
+const main = async () => {
+    const contractFactory = await hre.ethers.getContractFactory('StandardStorage');
+    const contract = await contractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.001"),
+    });
+
+    await contract.deployed();
+
+    console.log("Contract deployed to:", contract.address);
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+runMain();
